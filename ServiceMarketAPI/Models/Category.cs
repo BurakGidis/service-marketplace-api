1f936de129e5938121f6889c1995e8fda09b3d26
Enes Taş <entas063@gmail.com>
2025-12-15 15:20:16 +0300

    Add Category and ServiceListing entitys

---
+++ ServiceMarketAPI/Models/Category.cs
@@ -0,0 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace ServiceMarketAPI.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; } =string.Empty;
+
+      
+        public ICollection<ServiceListing> ServiceListings { get; set; }= null!;
+    }
+}
