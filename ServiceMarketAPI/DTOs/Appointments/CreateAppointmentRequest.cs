7ff05a945b66c7ec47bd151c7704c01672e5b88b
Ardil Sungu <ardil.sungu211@gmail.com>
2025-12-17 20:05:14 +0300

    Fix DateTime UTC issue in AppointmentsController

--- ServiceMarketAPI/DTOs/Appointments/CreateAppointmentRequest.cs
+++ ServiceMarketAPI/DTOs/Appointments/CreateAppointmentRequest.cs
@@ -2,11 +2,15 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace ServiceMarketAPI.DTOs.Appointments
 {
     public class CreateAppointmentRequest
     {
-        
+        [Required]
+        public int ServiceListingId { get; set; }
+        [Required]
+        public DateTime Date { get; set; }
     }
 }
